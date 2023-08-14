db28cc08256dd14556ec21f077750a637ac5c9c5
Mustafa Nedeer <mustafanedeer1@gmail.com>
2023-08-14 12:13:26 +0300

    Problems Solving With C++ Level One #33 - Video 33 - Codewars 8 Kyu - 3 Problems

--- elzero.c
+++ elzero.c
@@ -6,16 +6,16 @@
 #include <ctype.h>
 
 int compare(const void *a, const void *b);
-int sum_array(int* b, int numb);
-char* point(int a, int b);
-int seat_in_theater(int total_col, int total_row, int col, int row);
+int indexes(int* a, int length, int b);
+char get_grade(int* b, int length);
+int paper_work(int n, int m);
 
 /* STARTING OF MAIN FUNCTION */
 int main(void) {
     /* ALL VARIABLES */
     char char_array[] = "mustafa nedeer";
     char* str_array[] = {"body", "tails", "heads"};
-    int int_array[] = {1, 5, 2, 3, 4, 6, 7, 8, 9, 10};
+    int int_array[] = {84, 79, 85};
     int lenght = 3;           // integer array lenght
     char* string_arr[] = {"always", "wanna", "do", "best"};
     
@@ -31,7 +31,7 @@ int main(void) {
 
 
     /* Test For Integer */
-    int int_var = seat_in_theater(16, 11, 5, 3);
+    int int_var = paper_work(7, 9);
     printf("RETURN TYPE INTEGER: %d\n", int_var);
 
 
@@ -48,14 +48,14 @@ int main(void) {
 
 
     /* Test For Char */
-    char char_var = 'a';
+    char char_var = get_grade(int_array, 3);
     printf("RETURN TYPE CHARCATER IS: %c\n", char_var);
 
 
 
     /* Test For Char Array */
-    char* str_var = point(5, 3);
-    printf("RETURN TYPE STRING: %s\n", str_var);
+    char* str_var;
+    //printf("RETURN TYPE STRING: %s\n", str_var);
 
     /* Test For String Array */
     char** str_arr;
@@ -76,24 +76,31 @@ int compare(const void *a, const void *b)
     return x - y;
 }
 
-int sum_array(int* b, int numb) {
-    if (numb == 0) {
-        return 0;
+int indexes(int* a, int length, int b) {
+    if (b >= length) return -1;
+    return pow(a[b], b);
+}
+
+char get_grade(int* b, int length) {
+    int avg;
+    int count;
+    for (int i = 0; i < length; i++) {
+        count = count + b[i];
+    }
+    avg = count / length;
+    if (avg >= 90) {
+        return 'A';
+    } else if (avg >= 80) {
+        return 'B';
+    } else if (avg >= 70) {
+        return 'C';
+    } else if (avg >= 60) {
+        return 'D';
     } else {
-        return b[0] + sum_array(b + 1, numb - 1);
+        return 'F';
     }
 }
 
-char* point(int a, int b) {
-    if (a > b) {
-        return "Win";
-    } else if (b > a) {
-        return "Lose";
-    } else {
-        return "Draw";
-    }
-}
-
-int seat_in_theater(int total_col, int total_row, int col, int row) {
-    return (total_col - col + 1) * (total_row - row);
+int paper_work(int n, int m) {
+    return n < 0 || m < 0 ? 0 : n * m;
 }
