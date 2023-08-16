981c7b28a1f5cf06f2a5abf662775da823d49c5a
Mustafa Nedeer <mustafanedeer1@gmail.com>
2023-08-16 12:22:38 +0300

    Problems Solving With C++ Level One #34 - Video 34 - Codewars 8 Kyu - 3 Problems

--- elzero.c
+++ elzero.c
@@ -6,16 +6,16 @@
 #include <ctype.h>
 
 int compare(const void *a, const void *b);
-int indexes(int* a, int length, int b);
-char get_grade(int* b, int length);
-int paper_work(int n, int m);
+int rental_car_cost(int days);
+int* pipe_fix(int* arr, int length);
+int student_grade(int grade, int project);
 
 /* STARTING OF MAIN FUNCTION */
 int main(void) {
     /* ALL VARIABLES */
     char char_array[] = "mustafa nedeer";
     char* str_array[] = {"body", "tails", "heads"};
-    int int_array[] = {84, 79, 85};
+    int int_array[] = {1, 2, 3, 4, 9};
     int lenght = 3;           // integer array lenght
     char* string_arr[] = {"always", "wanna", "do", "best"};
     
@@ -31,16 +31,24 @@ int main(void) {
 
 
     /* Test For Integer */
-    int int_var = paper_work(7, 9);
+    int int_var = student_grade(85, 5);
     printf("RETURN TYPE INTEGER: %d\n", int_var);
 
 
 
+
+    /* Test For Float */
+    float fl_var = 7.23;
+    printf("RETURN TYPE FLOAT: %f\n", fl_var);
+
+
+
+
     /* Test For Integer Array */
-    int* arr_var = int_array;
+    int* arr_var = pipe_fix(int_array, 5);
     printf("RETURN TYPE ARRAY: [");
-    for (int i = 0; i < 2; i++) {
-        printf(" %d ", *(arr_var + i));
+    for (int i = 0; i < 9; i++) {
+        printf(" %d ", arr_var[i]);
     }
     printf("]\n");
 
@@ -48,7 +56,7 @@ int main(void) {
 
 
     /* Test For Char */
-    char char_var = get_grade(int_array, 3);
+    char char_var = 'A';
     printf("RETURN TYPE CHARCATER IS: %c\n", char_var);
 
 
@@ -76,31 +84,32 @@ int compare(const void *a, const void *b)
     return x - y;
 }
 
-int indexes(int* a, int length, int b) {
-    if (b >= length) return -1;
-    return pow(a[b], b);
-}
-
-char get_grade(int* b, int length) {
-    int avg;
-    int count;
-    for (int i = 0; i < length; i++) {
-        count = count + b[i];
-    }
-    avg = count / length;
-    if (avg >= 90) {
-        return 'A';
-    } else if (avg >= 80) {
-        return 'B';
-    } else if (avg >= 70) {
-        return 'C';
-    } else if (avg >= 60) {
-        return 'D';
+int rental_car_cost(int days) {
+    int cost = days * 40;
+    if (days >= 7) {
+        return cost - 50;
+    } else if(days >= 3) {
+        return cost - 20;
     } else {
-        return 'F';
+        return cost;
     }
 }
 
-int paper_work(int n, int m) {
-    return n < 0 || m < 0 ? 0 : n * m;
+int* pipe_fix(int* a, int length) {
+    int start = a[0];
+    int end = a[length - 1];
+    int diff = end - start + 1;
+    int * p = malloc(diff * sizeof(int));
+    for (int i = 0; i < diff; i++) {
+        p[i] = start;
+        start++;
+    }
+    return p;
+}
+
+int student_grade(int grade, int project) {
+    if (grade == 100 || project > 10) return 100;
+    if (grade > 75 && project >= 5) return 90;
+    if (grade > 50 && project >= 2) return 75;
+    return 0;
 }
