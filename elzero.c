e89da0499c7d3e8d5f4472b8d45549525fa524cf
Mustafa Nedeer <mustafanedeer1@gmail.com>
2023-08-26 09:54:27 +0300

    LeetCode longestCommonPrefix

--- elzero.c
+++ elzero.c
@@ -6,22 +6,18 @@
 #include <ctype.h>
 
 int compare(const void *a, const void *b);
-bool is_palindrome (int n);
-void reverse_string(char* string);
-int roman_to_int(char * s);
-
+char * longestCommonPrefix(char ** strs, int strsSize);
 /* STARTING OF MAIN FUNCTION */
 int main(void) {
     /* ALL VARIABLES */
     char char_array[] = "ma";
-    reverse_string(char_array);
-    char* str_array[] = {"body", "tails", "heads"};
+    char* str_array[] = {"dog", "flow", "flight"};
     int int_array[] = {3, 2, 2, 2, 3};
     int lenght = 3;           // integer array lenght
     char* string_arr[] = {"always", "wanna", "do", "best"};
     
     /* Test For Boolean */
-    bool bool_var = is_palindrome(1221);
+    bool bool_var = 1;
     if(bool_var) {
         printf("THE BOOLEAN VALUE IS: TRUE\n");
     } else{
@@ -32,7 +28,7 @@ int main(void) {
 
 
     /* Test For Integer */
-    int int_var = roman_to_int("III");
+    int int_var = 0;
     printf("RETURN TYPE INTEGER: %d\n", int_var);
 
 
@@ -63,8 +59,8 @@ int main(void) {
 
 
     /* Test For Char Array */
-    //char* str_var =;
-    printf("RETURN TYPE STRING: %s\n", char_array);
+    char* str_var = longestCommonPrefix(str_array, 3);
+    printf("RETURN TYPE STRING: %s\n", str_var);
 
     /* Test For String Array */
     /*char** str_arr = char_array;
@@ -87,89 +83,23 @@ int compare(const void *a, const void *b)
     return x - y;
 }
 
-bool is_palindrome (int n) {
-    char arr[12];
-    char rev[12];
-    int length = 0;
-    int j = 0;
+char * longestCommonPrefix(char ** strs, int strsSize) {
+    char * commonpre = malloc(200 * sizeof(char));
+    int index = 0;
     int i;
-    sprintf(arr, "%d", n);
-    for (i = 0; arr[i] != '\0'; i++) {
-        length++;
-    }
-    for (i = length - 1; i >= 0; i--) {
-        rev[j] = arr[i];
-        j++;
-    }
-    rev[j] = '\0';
-    
-    return (strcmp(arr, rev) == 0) ? 1 : 0;
-}
-
-void reverse_string(char* string) {
-    int length = 0;
-    int i;
-    char temp;
-    for (i = 0; string[i] != '\0'; i++) {
-        length++;
-    }
-    if (length == 1) return;
-    for (i = 0; i <= floor(length / 2) - 1; i++) {
-        temp = string[length - i - 1];
-        string[length - i - 1] = string[i];
-        string[i] = temp;
-    }
-}
-
-int roman_to_int(char * s) {
-    if (s[0] == '\0') {
-
-        return 0;
-
-    }else if (s[0] == 'M') {
-
-        return 1000 + roman_to_int(s + 1);
-
-    } else if (s[0] == 'D') {
-
-        return 500 + roman_to_int(s + 1);
-
-    } else if (s[0] == 'C') {
-
-        if (s[1] == 'M') {
-            return 900 + roman_to_int(s + 2);
-        } else if (s[1] == 'D'){
-            return 400 + roman_to_int(s + 2);
-        } else {
-            return 100 + roman_to_int(s + 1);
+    while (true) {
+        char c = strs[0][index];   /* the indexed character in the first string */
+        if (c == '\0') {           /* just in case the strings are all equal */
+            commonpre[index] = '\0';
+            return commonpre;
         }
-
-    } else if (s[0] == 'L') {
-
-       return 50 + roman_to_int(s + 1);
-
-    } else if (s[0] == 'X') {
-
-        if (s[1] == 'C') {
-            return 90 + roman_to_int(s + 2);
-        } else if (s[1] == 'L'){
-            return 40 + roman_to_int(s + 2);
-        } else {
-            return 10 + roman_to_int(s + 1);
+        for (i = 1; i < strsSize; i++) {
+            if (strs[i][index] != c) {
+                commonpre[index] = '\0';
+                return commonpre;
+            }
         }
-
-    } else if (s[0] == 'V') {
-
-        return 5 + roman_to_int(s + 1);
-
-    } else if (s[0] == 'I') {
-
-        if (s[1] == 'X') {
-            return 9 + roman_to_int(s + 2);
-        } else if (s[1] == 'V'){
-            return 4 + roman_to_int(s + 2);
-        } else {
-            return 1 + roman_to_int(s + 1);
-        }
+        commonpre[index] = c;
+        index++;
     }
 }
