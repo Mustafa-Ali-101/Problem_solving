13c7200dd10d101983a8f17d425f2cb5e0838e8a
Mustafa Nedeer <mustafanedeer1@gmail.com>
2024-01-14 13:31:15 +0200

    Problem Solving

--- main.c
+++ main.c
@@ -5,347 +5,410 @@
 #include <string.h>
 #include <ctype.h>
 #include "imp_list.h"
-//#include "hash.h"
+#include "hash_imp.h"
 
-typedef struct treenode {
+typedef struct treenode
+{
   int val;
-  struct treenode* left;
-  struct treenode* right;
+  struct treenode
+      *left;
+  struct treenode
+      *right;
 } treenode;
 
-node* two_sorted(node* list1, node* list2);
-treenode* make_node(int n);
-bool insert(treenode** root, int n);
-bool search(treenode* root, int n);
-void depth_first(treenode* root, int length);
-void depth_recursive(treenode* root);
-void breadth_first(treenode* root, int n);
-int* inorderTraversal(treenode* root, int* returnSize);
-bool isSymmetricEqual(treenode* root1, treenode* root2);
-int sumTree(treenode* root);
-int maxOfThree(int a, int b, int c);
-int maxNode(treenode* root);
-bool canSum(int target, int* list, int listLength);
-void appendNewNode(node** head, int val);
-node* reverseLinkedList (node* head);
-bool isArrayPal(int* li, int length);
-node* recFun(node* l1, node* l2, int over);
+typedef struct integerNode {
+  int value;
+  struct integerNode* next;
+} intNode;
+
+int *getRowRecursive(int rowIndex, int *returnSize);
+int *getRow(int rowIndex, int *returnSize);
+bool isPalindrome(char *s);
+bool isAlphaNum(char c);
+char lower(char big);
+int *preorderTraversal(treenode *root, int *returnSize);
+char *convertToTitle(int columnNumber);
+int titleToNumber(char* columnTitle);
+intNode** makeHashTable(int size);
+intNode* makeInt (int x);
+void addToHashTable(intNode** h,int i, int len);
+bool isInTable(int x, intNode** h,int len);
+bool containsDuplicate(int* nums, int numsSize);
+void moveZeroes(int* nums, int numsSize);
 
 
-int main() {
+int main()
+{
+  int nums[] = {1, 0, 0, -4, 5, 6, 7, 8, 9};
+
+  moveZeroes(nums, 9);
+  
+
+  for (int i = 0; i < 9; i++) {
+    printf("%d\n", nums[i]);
+  }
   return 0;
 }
 
+// I should Use Memoization because tha Algorithm is Very Slow
+int *getRowRecursive(int rowIndex, int *returnSize)
+{
+  if (rowIndex == 0)
+  {
+    *returnSize = 1;
+    int *returnedArray = malloc(sizeof(int));
+    returnedArray[0] = 1;
+    return returnedArray;
+  }
+  else
+  {
+    int theSize;
+    *returnSize = rowIndex + 1;
+    int *returnedArray = malloc((rowIndex + 1) * sizeof(int));
+    returnedArray[0] = 1;
+    returnedArray[rowIndex] = 1;
+    for (int i = 1; i < rowIndex; i++)
+    {
+      returnedArray[i] = getRowRecursive(rowIndex - 1, &theSize)[i - 1] + getRowRecursive(rowIndex - 1, &theSize)[i];
+    }
 
-node* two_sorted(node* list1, node* list2) {
-  node* queue = new_node(0);
-  node* temp1 = list1;
-  node* temp2 = list2;
-  while (temp1 != NULL || temp2 != NULL) {
-    /* if one of them is null */
-    if (temp1 == NULL) { 
-      append(&queue, temp2->val);
-      temp2 = temp2->next;
-    } else if (temp2 == NULL) {
-      append(&queue, temp1->val);
-      temp1 = temp1->next;
-    } else { // if none of them is null
-      if (temp1->val > temp2->val) {
-        append(&queue, temp2->val);
-        temp2 = temp2->next; 
-      } else if (temp1->val < temp2->val) {
-        append(&queue, temp1->val);
-        temp1 = temp1->next;        
-      } else {
-        append(&queue, temp2->val);
-        append(&queue, temp1->val);
-        temp1 = temp1->next;
-        temp2 = temp2->next;
-      }
+    return returnedArray;
+  }
+}
+
+int *getRow(int rowIndex, int *returnSize)
+{
+  int index = rowIndex + 1;
+  int **arrayOfArrays = malloc(index * sizeof(int *));
+
+  // set the first array which is the base case
+  arrayOfArrays[0] = malloc(sizeof(int));
+  arrayOfArrays[0][0] = 1;
+
+  // looping over the arrayOfArrays to set the inner arrays
+  for (int i = 1; i < index; i++)
+  {
+    arrayOfArrays[i] = malloc((i + 1) * sizeof(int));
+
+    // set the first and last items in the inner array
+    arrayOfArrays[i][0] = 1;
+    arrayOfArrays[i][i] = 1;
+
+    // looping over the inner array
+    for (int j = 1; j < i; j++)
+    {
+      arrayOfArrays[i][j] = arrayOfArrays[i - 1][j - 1] + arrayOfArrays[i - 1][j];
     }
   }
 
-  return queue->next;
+  *returnSize = index;
+  return arrayOfArrays[rowIndex];
 }
 
-treenode* make_node(int n) {
-  treenode* r = malloc(sizeof(treenode));
-  r->left = NULL;
-  r->right = NULL;
-  r->val = n;
-
-  return r;
+bool isAlphaNum(char c)
+{
+  return (((int)c >= 65 && (int)c <= 90) || ((int)c >= 97 && (int)c <= 122) || ((int)c >= 48 && (int)c <= 57));
 }
 
-bool insert(treenode** root, int n) {
-  if ((*root) == NULL) {
-    (*root) =  make_node(n);
-    return true;
-  } else if ((*root)->val == n) {
+char lower(char big)
+{
+  int ascii = (int)big;
+  if (ascii >= 65 && ascii <= 90)
+  {
+    ascii += 32;
+    return (char)ascii;
+  }
+  else
+  {
+    return big;
+  }
+}
+
+bool isPalindrome(char *s)
+{
+  // First we have to know the length of the string
+  int len = 0;
+  for (int i = 0; s[i] != '\0'; i++)
+  {
+    len++;
+  }
+
+  // Then we make two integers one at the begging and one at the end
+  int start = 0;
+  int end = len - 1;
+
+  // All the work in here
+  while (start != end && end > start)
+  {
+    if (!isAlphaNum(s[start]))
+    {
+      start++;
+      continue;
+    }
+    else if (!isAlphaNum(s[end]))
+    {
+      end--;
+      continue;
+    }
+    else
+    {
+      if (lower(s[start]) != lower(s[end]))
+        return false;
+      start++;
+      end--;
+    }
+  }
+  return true;
+}
+
+bool hasCycle(node *head)
+{
+  if (head == NULL)
     return false;
-  } else if ((*root)->val > n) {
-    insert(&((*root)->left), n);
-    return true;
-  } else {
-    insert(&((*root)->right), n);
-    return true;   
-  }
 
-}
+  node *turtle = head;
+  node *tale = head;
 
-bool search(treenode* root, int n) {
-  if (root == NULL) {
-    return false;
-  } else if(root->val == n) {
-    return true;
-  } else if(root->val > n) {
-    return search(root->left, n);
-  } else {
-    return search(root->right, n);
-  }
-}
+  while (tale->next != NULL && tale->next->next != NULL)
+  {
+    tale = tale->next->next;
+    turtle = turtle->next;
 
-void depth_first(treenode* root, int length) {
-  treenode* stack[length];
-  int counter = 1;
-  stack[counter] = root;
-  while(counter > 0) {
-    treenode* current = stack[counter];
-    printf("%d\n", current->val);
-    counter--;
-    if (current->right != NULL) {
-      counter++;
-      stack[counter] = current->right;
-    }
-    if (current->left != NULL) {
-      counter++;
-      stack[counter] = current->left;
-      //counter++;
-    }
-  }
-}
-
-void depth_recursive(treenode* root) {
-  treenode* temp = root;
-  if (temp != NULL) {
-    printf("%d\n", temp->val);
-    depth_recursive(temp->left);
-    depth_recursive(temp->right);
-  }
-}
-
-void breadth_first(treenode* root, int n) {
-  treenode * queue[n];
-  int counter = 0;
-  int index = 0;
-  queue[counter] = root;
-  //counter++;
-  while (counter >= index) {
-    treenode* current = queue[index];
-    index++;
-    printf("%d\n", current->val);
-    if (current->left != NULL) {
-      counter++;
-      queue[counter] = current->left;
-    }
-    if (current->right != NULL) {
-      counter++;
-      queue[counter] = current->right;
-    }
-  }
-}
-
-int* inorderTraversal(treenode* root, int* returnSize) {
-    if (root == NULL) {
-        *returnSize = 0;
-        //int* empty = NULL;
-        return NULL;
-    } else {
-        int leftSize;
-        int rightSize;
-
-        // recursive calls to the subtrees
-        int * left = inorderTraversal(root->left, &leftSize);
-        int * right = inorderTraversal(root->right, &rightSize);
-
-        // make the big list
-        *returnSize = leftSize + rightSize + 1;
-        int*  notEmpty = malloc(sizeof(int) /** (*returnSize)*/);
-
-
-        // add the left subtree to the list
-        int j = 0;
-        for (; j < leftSize; j++) {
-          notEmpty[j] = left[j];
-        }
-
-
-        // adding the root val to the list
-        notEmpty[j] = root->val;
-        j++;
-
-
-        // add the right subtree to the list
-        for (int i = 0; i < rightSize; i++) {
-          notEmpty[i + j] = right[i];
-        }
-
-
-        free(left);
-        free(right);
-        return notEmpty;
-    }
-}
-
-bool isSameTree(treenode* p, treenode* q) {
-    if (p == NULL && q == NULL) {
+    if (tale == turtle)
       return true;
-    } else if (p == NULL){
-      return false;
-    } else if (q == NULL) {
-      return false;
-    } else {
-      return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-    }
-}
-
-bool isSymmetricEqual(treenode* root1, treenode* root2) {
-    if (root1 == NULL && root2 == NULL) {
-      return true;
-    } else if (root1 != NULL && root2 == NULL) {
-      return false;
-    } else if (root2 != NULL && root1 == NULL) {
-      return false;
-    } else {
-      return root1->val == root2->val && isSymmetricEqual(root1->left, root2->right) && isSymmetricEqual(root1->right, root2->left);
-    }
-}
-
-int sumTree(treenode* root) {
-  if (root == NULL) {
-    return 0;
-  } else {
-    return root->val + sumTree(root->left) + sumTree(root->right);
-  }
-}
-
-int maxOfThree(int a, int b, int c) {
-  if (a > b) {
-    if (a > c) {
-      return a;
-    } else {
-      return c;
-    }
-  } else {
-    if (b > c) {
-      return b;
-    } else {
-      return c;
-    }
-  }
-}
-
-int maxNode(treenode* root) {
-  if (root == NULL) {
-    return 0;
-  } else {
-    return maxOfThree(root->val, maxNode(root->left), maxNode(root->right));
-  }
-}
-
-void merge(node* list1, node* list2,node* rr) {
-    if (list1 == NULL) { rr->next = list2;}
-    else if (list2 == NULL) { rr->next = list1;}
-    else {
-        node* NewNode = malloc(sizeof(node));
-        if (list1->val >= list2->val) {
-            NewNode->val = list1->val;
-            merge(list1->next, list2, rr->next);
-        } else {
-            NewNode->val = list2->val;
-            merge(list1, list2->next, rr->next);            
-        }
-    }
-}
-
-node* mergeTwoLists(node* list1, node* list2) {
-    node* ReturnPointer = malloc(sizeof(node));
-    merge(list1, list2, ReturnPointer);
-
-    return ReturnPointer->next;
-}
-
-bool canSum(int target, int* list, int listLength) {
-  if (target == 0) return true;
-  if (target < 0) return false;
-
-  for (int i = 0; i < listLength; i++) {
-    if (canSum(target - list[i], list, listLength)) return true;
   }
 
   return false;
 }
 
-void appendNewNode(node** head, int val) {
-    node* temp = *head;
-    node* newNode = malloc(sizeof(node));
-    newNode->val = val;
-    newNode->next = NULL;
-    
-
-    if (temp == NULL) {
-        *head = newNode;
-        //return newNode;
-    } else {
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-    }   
-}
-
-node* reverseLinkedList (node* head) {
-    if (head == NULL) {
-        return NULL;
-    } else {
-        node* new = reverseLinkedList(head->next);
-        appendNewNode(&new, head->val);
-        return new;
-    }
-}
-
-bool isArrayPal(int* li, int length) {
-  int first = 0;
-  int last = length - 1;
-  while (last > first) {
-    if (li[last] != li[first]) return false;
-    last--;
-    first++;
-  }
-
-  return true;
-}
-
-node* recFun(node* l1, node* l2, int over) {
-  if (over == 0 && l1 == NULL && l2 == NULL) {
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *preorderTraversal(treenode *root, int *returnSize)
+{
+  if (root == NULL)
+  {
+    *returnSize = 0;
     return NULL;
-  } else {
-    node* newNode = malloc(sizeof(node));
+  }
+  else
+  {
+    int left;
+    int right;
+    int *leftArray = preorderTraversal(root->left, &left);
+    int *rightArray = preorderTraversal(root->right, &right);
 
+    *returnSize = left + right + 1;
+    int *returnedArray = malloc(sizeof(int) * (*returnSize));
 
-    int digit = over;
-    if (l1 != NULL) {digit += l1->val; l1 = l1->next;}
-    if (l2 != NULL) {digit += l2->val; l2 = l2->next;}
+    returnedArray[0] = root->val;
 
-    if (digit < 10) {
-      newNode->val = digit;
-      newNode->next = recFun(l1->next, l2->next, 0);
-      return newNode;
-    } else {
-      newNode->val = digit - 10;
-      newNode->next = recFun(l1->next, l2->next, 1);
-      return newNode;
+    for (int i = 0; i < left; i++)
+    {
+      returnedArray[i + 1] = leftArray[i];
     }
+
+    for (int i = 0; i < right; i++)
+    {
+      returnedArray[left + i] = rightArray[i];
+    }
+
+    return returnedArray;
   }
 }
 
+int *postorderTraversal(treenode *root, int *returnSize)
+{
+  if (root == NULL)
+  {
+    *returnSize = 0;
+    return NULL;
+  }
+  else
+  {
+    int left;
+    int right;
+    int sum;
+    int *leftArray = postorderTraversal(root->left, &left);
+    int *rightArray = postorderTraversal(root->right, &right);
+
+    sum = left + right + 1;
+    *returnSize = sum;
+    int *returnedArray = malloc(sizeof(int) * (sum));
+
+    returnedArray[sum - 1] = root->val;
+
+    for (int i = 0; i < left; i++)
+    {
+      returnedArray[i] = leftArray[i];
+    }
+
+    for (int i = 0; i < right; i++)
+    {
+      returnedArray[left + i] = rightArray[i];
+    }
+
+    free(leftArray);
+    free(rightArray);
+    return returnedArray;
+  }
+}
+
+node *getIntersectionNode(node *headA, node *headB)
+{
+  node *tempA = headA;
+  node *tempB = headB;
+
+  while (tempA != NULL)
+  {
+    while (tempB != NULL)
+    {
+      if (tempA == tempB)
+      {
+        return tempA->val;
+      }
+      tempB = tempB->next;
+    }
+    tempA = tempA->next;
+  }
+
+  return NULL;
+}
+
+char* convertToTitle(int columnNumber) {
+  // allocate the alpha array and make it all null
+  char* returned = NULL;
+
+
+  // loop to convert the number to letters
+  int len = 0;
+  while(columnNumber > 0) {
+    // remainder of the number divided by 26
+    int alpha = (columnNumber - 1) % 26;
+
+
+    // quotient of the number divided by 26
+    columnNumber = (columnNumber - 1) / 26;
+
+
+    // allocate one more space for the output array
+    returned = realloc(returned, (len + 1) * sizeof(char));
+
+    // assign the letter to the output array
+    returned[len] = 'A' + alpha;
+
+    len++;
+  }
+
+
+  // reverse the output array
+  for(int i = 0; i < len / 2; i++) {
+    char temp = returned[i];
+    returned[i] = returned[len - i - 1];
+    returned[len - i - 1] = temp;
+  }
+
+  // this is to add a null terminator to the output array
+  returned = realloc(returned, sizeof(char) * (len + 1));
+  returned[len] = '\0';
+  return returned;
+}
+
+int titleToNumber(char* columnTitle) {
+    int len = 0;
+    int total = 0;
+
+    // set length of the string given
+    for(int i = 0; columnTitle[i] != '\0'; i++) {
+      len++;
+    }
+
+    // caluculate the total number
+    for(int i = len - 1; i >= 0; i--) {
+      total = total + (((int) columnTitle[i]) - 64) * pow(26, (len - 1) - i);
+    }
+
+    return total;
+}
+
+
+/* return a pointer to the hash table of pointers to intNodes */
+intNode** makeHashTable(int size) {
+
+    intNode** h =(intNode**) malloc(size * sizeof(intNode*));
+
+    /* initializing the hash table */
+    for (int i = 0; i < size; i++) {
+        h[i] = NULL;
+    }
+    return h;
+}
+
+intNode* makeInt (int x) {
+  intNode* result = malloc(sizeof(intNode));
+  result->value = x;
+  result->next = NULL;
+  return result;
+}
+
+void addToHashTable(intNode** h,int i, int len) {
+  intNode* iNode = makeInt(i);
+
+  int index = abs((iNode->value)) % len;
+  
+  //intNode* temp = p;
+  iNode->next = h[index];
+  h[index] = iNode;
+}
+
+bool isInTable(int x, intNode** h,int len) {
+  int index = abs(x) % len;
+  intNode* temp = h[index];
+
+  while(temp != NULL) {
+    if (temp->value == x) {
+      return true;
+    }
+    temp = temp->next;
+  }
+  return false;
+}
+
+
+bool containsDuplicate(int* nums, int numsSize) {
+  int tableLength = numsSize;
+  intNode** table = makeHashTable(tableLength);
+
+  for (int i = 0; i < numsSize; i++) {
+    if (isInTable(nums[i],table, tableLength)) {
+      return true;
+    } else {
+      addToHashTable(table, nums[i], tableLength);
+    }
+  }
+
+  return false;
+}
+
+void moveZeroes(int* nums, int numsSize) {
+  int leftPointer = numsSize;
+  int rightPointer = numsSize;
+
+
+  // setting the left and right
+  for (int i = 0; i < numsSize; i++) {
+    if (nums[i] == 0) {
+      leftPointer = i;
+      rightPointer = i + 1;
+      break;
+    }
+  }
+
+  for (rightPointer; rightPointer < numsSize; rightPointer++) {
+    if (nums[rightPointer]) {
+      nums[leftPointer] = nums[rightPointer];
+      nums[rightPointer] = 0;
+      leftPointer++;
+    }
+  }
+}
